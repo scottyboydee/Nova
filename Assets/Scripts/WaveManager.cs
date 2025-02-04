72c55bed9c824214e65bb472cd1e0928630ce1b5
Scott Lamb <deathboy@deathboy.co.uk>
2025-02-04 20:36:24 +0000

    Continuing work on the wave manager. it now loads 3 demo waves (all at once, just for testing)

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject waveParent;
+
     [SerializeField] 
     private SO_WaveSet waveSet;
 
@@ -23,15 +26,19 @@ public class WaveManager : MonoBehaviour
         foreach (Wave wave in waveSet.waves)
         {
             Debug.Log("Creating Wave: " + wave.name);
-
+/*
             if (wave.wavePrefab == null)
             {
                 Debug.LogError("WavePrefab is NULL");
                 continue;
             }
-
-            Debug.Log("Spawning Wave: " + wave.wavePrefab.name);
-            Instantiate(wave.wavePrefab, Vector3.zero, Quaternion.identity);
+*/
+//            Debug.Log("Spawning Wave: " + wave.wavePrefab.name);
+            Instantiate(wave, Vector3.zero, Quaternion.identity);
+            GameObject newWave = wave.gameObject.Clone(waveParent.transform, wave.name, Vector3.zero);
+            RectTransform rectTransform = (RectTransform)newWave.transform;
+            rectTransform.anchoredPosition = Vector2.zero;
+            //            useShot = newShot.GetComponent<PlayerShot>();
         }
 
     }
