941c137306786cae345759a86b8706d1ffdc6a47
Scott Lamb <deathboy@deathboy.co.uk>
2025-03-18 17:40:54 +0000

    Level 9 (with top and bottom arcing baddies) now complete!

--- Assets/Scripts/BaddiePathFollower.cs
+++ Assets/Scripts/BaddiePathFollower.cs
@@ -10,17 +10,27 @@ public class BaddiePathFollower : MonoBehaviour
     [SerializeField]
     private float Speed;
 
+    [SerializeField]
+    private float Delay = 0;
+
+    private float delayRemaining;
+
     private float progress;
 
     // Start is called before the first frame update
     void Start()
     {
+        delayRemaining = Delay;
         progress = 0;        
     }
 
     // Update is called once per frame
     void Update()
     {
+        delayRemaining -= Time.deltaTime;
+        if (delayRemaining > 0)
+            return;
+
         UpdatePositionFromPath();
     }
 
