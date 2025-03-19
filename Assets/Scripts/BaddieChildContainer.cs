ed4a78c4fba64b7c4d68adc8fb09cdf36ff975a0
Scott Lamb <deathboy@deathboy.co.uk>
2025-03-19 16:37:43 +0000

    Refactoring and generalising the LadybugBabyContainer to permit easier reuse when I need the same functionality for the Bag of Bouncers

--- Assets/Scripts/BaddieChildContainer.cs
+++ Assets/Scripts/BaddieChildContainer.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LadybugBabyContainer : MonoBehaviour, IDeathAction
+public class BaddieChildContainer : MonoBehaviour, IDeathAction
 {
     [SerializeField]
-    private BaddieVertSine[] baby;
+    private GameObject[] baby;
 
     public void Died()
     {
